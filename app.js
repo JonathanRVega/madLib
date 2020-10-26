87ab74dcf8fba9d3b8ef05205370e7536be52e56
JonathanRVega <Jonathaan.rv@gmail.com>
2020-10-26 08:21:21 -0700

    Changed the button and gave it an animation, justified text and fixed grammar

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ function createParagraph() {
     var paragraph = '<p>It was a hot day. In fact it was so hot you could have cooked ' +wordTwo+' on the sidewalk.'+
     'Squigly sat on his front porch trying to eat his ' +wordFive+ ' before it melted.' +
     'Just as Squigly was about to go inside, he saw ' + wordOne + ' coming down the street.'+wordOne+' was skipping so '+
-    +wordFour+ ' that Squigly was sure '+wordOne+' would faint from the heat. When '+wordOne+ ' reached his front porch,'+
-    'Squigly noticed that ' +wordOne+ ' wasnt even sweating.'+
+    wordFour+ ' that Squigly was sure '+wordOne+' would faint from the heat. When '+wordOne+ ' reached his front porch,'+
+    'Squigly noticed that ' +wordOne+ ' wasn\'t even sweating.'+
     'How can you not be hot? asked Squigly, wiping the sweat from his forehead.'+
-    'Hot, Im not hot, Im wearing my invention said ' +wordOne+ '. '+
+    'Hot, I\'m not hot, I\'m wearing my invention said ' +wordOne+ '. '+
     'What kind of invention is that? asked Squigly.'+
     'Frozen ' +wordThree+ ' of course, I heard it was going to be hot today so I just put'+
-    'my ' +wordThree+ ' in the freezer last night and today I feel great! It really works, you should try it.</p>';
+    ' my ' +wordThree+ ' in the freezer last night and today I feel great! It really works, you should try it.</p>';
 
     document.getElementById('answer').innerHTML = paragraph
 
